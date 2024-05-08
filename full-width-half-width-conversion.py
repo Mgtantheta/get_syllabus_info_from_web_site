f1c5b373a798fb2c4975768d26d85633b8e8da4f
Mgtantheta <freecityNJ@gmail.com>
2024-05-08 21:49:37 +0900

    pathの変更

--- full-width-half-width-conversion.py
+++ full-width-half-width-conversion.py
@@ -1,7 +1,9 @@
 import pandas as pd
 
+
+path = '/Users/hat/Developer/Python/get_syllabus_info_from_web_site/syllabus_data.csv'
 # csvファイルを読み込む
-df = pd.read_csv('/Users/hat/Developer/Python/get_syllabus_info_from_web_site-1/data.csv', header=None)
+df = pd.read_csv(path, header=None)
 
 # 全角数字を半角数字に変換する
 df = df.applymap(lambda x: str(x).translate(str.maketrans({chr(0xFF10 + i): chr(0x30 + i) for i in range(10)})))
@@ -10,4 +12,4 @@ df = df.applymap(lambda x: str(x).translate(str.maketrans({chr(0xFF10 + i): chr(
 df = df.applymap(lambda x: str(x).translate(str.maketrans({chr(0xFF1A): chr(0x3A)})))
 
 # 変換後のデータをcsvファイルに書き込む
-df.to_csv('/Users/hat/Developer/Python/get_syllabus_info_from_web_site-1/data.csv', index=False, header=False)
+df.to_csv(path, index=False, header=False)
