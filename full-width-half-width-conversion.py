d1853b11bf65f30ade70aeb6c335a22c5c8caaf4
Haruto Muraki <114408959+Mgtantheta@users.noreply.github.com>
2024-05-13 16:36:04 +0900

    add: CSVを綺麗にする

--- full-width-half-width-conversion.py
+++ full-width-half-width-conversion.py
@@ -1,15 +1,33 @@
 import pandas as pd
-
+import re
 
 path = '/Users/hat/Developer/Python/get_syllabus_info_from_web_site/syllabus_data.csv'
 # csvファイルを読み込む
 df = pd.read_csv(path, header=None)
 
-# 全角数字を半角数字に変換する
-df = df.applymap(lambda x: str(x).translate(str.maketrans({chr(0xFF10 + i): chr(0x30 + i) for i in range(10)})))
+# 不要な空白の削除
+df = df.map(lambda x: re.sub(r'\s+', ' ', str(x).strip()))
 
-# 全角の：を半角に変換する
-df = df.applymap(lambda x: str(x).translate(str.maketrans({chr(0xFF1A): chr(0x3A)})))
+# 改行コードの統一
+df = df.map(lambda x: re.sub(r'\r\n|\r|\n', '\n', str(x)))
+
+# 欠損値の処理
+df = df.fillna('')
+
+# 全角英数字を半角英数字に変換
+df = df.map(lambda x: str(x).translate(str.maketrans({chr(0xFF01 + i): chr(0x21 + i) for i in range(94)})))
+
+# 全角記号を半角記号に変換
+df = df.map(lambda x: str(x).translate(str.maketrans({chr(0xFF01 + i): chr(0x21 + i) for i in range(94)})))
+
+# カテゴリデータの処理
+df = df.map(lambda x: re.sub(r'必修選択', '選択必修', str(x)))
+
+# 重複データの削除
+df = df.drop_duplicates()
+
+# データ型の変換
+df = df.map(lambda x: pd.to_numeric(x, errors='ignore'))
 
 # 変換後のデータをcsvファイルに書き込む
 df.to_csv(path, index=False, header=False)
